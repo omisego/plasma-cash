508f579b9f1bb57ba5c8501a3700383fffd0cd33
HuangJi <a1345962293@gmail.com>
2018-04-24 15:17:01 +0800

    Add integration tests folder and erc20 contract.
    - integration tests will be used for testing multi token deposit.
    - erc 20 contract is from the ethereum wiki version.

--- deployment.py
+++ deployment.py
@@ -1,3 +1,5 @@
 from plasma_cash.root_chain.deployer import Deployer
+from integration_tests.erc20.deployer import Erc20Deployer
 
 Deployer().deploy_contract('RootChain/RootChain.sol')
+Erc20Deployer().deploy_contract('PlasmaCashToken/PlasmaCashToken.sol')
